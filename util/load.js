a3357f0859cc687ad650dc487de5f7fd89276c99
Brad Egler <begler@gmail.com>
2014-02-27 08:47:47 -0500

    load changes

--- util/load.js
+++ util/load.js
@@ -2,7 +2,9 @@
 
 'use strict';
 
+var lazy = require('lazy');
 var fs = require('fs');
+
 var program = require('commander');
 var async = require('async');
 var Person = require('../server/lib/person').Person;
@@ -13,62 +15,62 @@ program
     .option('-t, --type [type]', 'file type [json, pipe]', 'json')
     .parse(process.argv);
 
-fs.readFile(program.file, {
-    encoding: 'utf-8'
-}, function(err, data) {
-    var persons;
-    if (program.type === 'json') {
-        persons = handleJson(data);
-    } else if (program.type === 'pipe') {
-        persons = handlePipe(data);
-    }
 
-    var actions = [];
-    persons.forEach(function(person) {
-        actions.push(function(callback) {
-            new Person(person).insert(function(err) {
-                if (err) console.error(err);
-                callback();
-            });
-        });
-    });
-    async.parallel(actions, function(err) {
-        if (err) console.error(err);
-        process.exit(0);
-    });
-});
-
-function handlePipe(data) {
-    var persons = [];
-    var lines = data.split('\n');
-    lines.forEach(function(line) {
-        var parts = line.split('|');
-        var phone;
-        if (parts[9] !== undefined && parts[10] !== undefined && parts[9].length > 0 && parts[10].length > 0) {
-            phone = '(' + parts[9] + ') ' + parts[10].substr(0, 3) + '-' + parts[10].substr(3);
-        }
-        persons.push({
-            id: parts[0],
-            name: {
-                last: parts[1],
-                first: parts[2],
-                mid: parts[3],
-            },
-            dob: parts[4],
-            address: {
-                line: parts[5],
-                city: parts[6],
-                state: parts[7],
-                zip: parts[8]
-            },
-            phone: phone
-        });
-    });
-    return persons;
+if (program.type === 'json') {
+    handleJson(program.file);
+} else if (program.type === 'pipe') {
+    handlePipe(program.file);
 }
 
-function handleJson(data) {
-    var obj = JSON.parse(data);
-    var persons = obj.persons;
-    return persons;
+function handlePipe(file, fn) {
+    new lazy(fs.createReadStream(file)).lines
+        .forEach(function(line) {
+            var parts = line.toString().split('|');
+            var phone;
+            if (parts[9] !== undefined && parts[10] !== undefined && parts[9].length > 0 && parts[10].length > 0) {
+                phone = '(' + parts[9] + ') ' + parts[10].substr(0, 3) + '-' + parts[10].substr(3);
+            }
+            var person = {
+                id: parts[0],
+                name: {
+                    last: parts[1],
+                    first: parts[2],
+                    mid: parts[3],
+                },
+                dob: parts[4],
+                address: {
+                    line: parts[5],
+                    city: parts[6],
+                    state: parts[7],
+                    zip: parts[8]
+                },
+                phone: phone
+            };
+            new Person(person).insert(function(err) {
+                if (err) console.error(err);
+            });
+        });
+    //process.exit(0);
+}
+
+function handleJson(file) {
+    fs.readFile(file, {
+        encoding: 'utf-8'
+    }, function(err, data) {
+        var obj = JSON.parse(data);
+        var persons = obj.persons;
+        var actions = [];
+        persons.forEach(function(person) {
+            actions.push(function(callback) {
+                new Person(person).insert(function(err) {
+                    if (err) console.error(err);
+                    callback();
+                });
+            });
+        });
+        async.parallel(actions, function(err) {
+            if (err) console.error(err);
+            process.exit(0);
+        });
+    });
 }
